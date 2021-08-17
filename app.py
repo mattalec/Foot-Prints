debfbb025c090050e9a48e79902178fca17410ce
Alec Matthews <alec.philip.matthews@gmail.com>
2021-08-17 20:30:16 +0100

    bug-fixes

--- app.py
+++ app.py
@@ -6,69 +6,41 @@ import webbrowser
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-# db = SQLAlchemy(app)
-
 ds = pd.read_csv('scripts.csv')
-
-# class Scripts(db.Model):
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	dob = db.Column(db.String(200), nullable=False)
-# 	name = db.Column(db.String(200), nullable=False)
-# 	email = db.Column(db.String(200), nullable=False)
-# 	medication = db.Column(db.String(200), nullable=False)
-# 	dose = db.Column(db.String(200), nullable=False)
-# 	quantity = db.Column(db.String(200), nullable=False)
-# 	time = db.Column(db.DateTime, default=datetime.utcnow)
-# 	date = db.Column(db.DateTime, default=datetime.utcnow)
-
-# 	def __repr__(self): 
-# 		return '<Script %r>' % self.id
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 	if request.method == 'POST':
 		task_content = request.form['content']
-		# new_task = Todo(content=task_content)
-
-		# try:
-		# 	db.session.add(new_task)
-		# 	db.session.commit()
-		# 	return redirect('/')
-		# except:
-		# 	return 'There was an issue adding your task'
-
 	else:
 		scripts = [row for row in ds.iterrows()]
-		# scripts = Scripts.query.order_by(Scripts.date).all()
 		return render_template('index.html', scripts=scripts)
 
-@app.route('/delete/<int:id>')
-def delete(id):
-	task_to_delete = Todo.quem
-	y.get_or_404(id)
+# @app.route('/delete/<int:id>')
+# def delete(id):
+# 	task_to_delete = Todo.quem
+# 	y.get_or_404(id)
+# 	try:
+# 		db.session.delete(task_to_delete)
+# 		db.session.commit()
+# 		return redirect('/')
+# 	except:
+# 		return 'There was a problem deleting that task'
 
-	try:
-		db.session.delete(task_to_delete)
-		db.session.commit()
-		return redirect('/')
-	except:
-		return 'There was a problem deleting that task'
+# @app.route('/update/<int:id>', methods=['GET', 'POST'])
+# def update(id):
+# 	task = Todo.query.get_or_404(id)
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-	task = Todo.query.get_or_404(id)
+# 	if request.method == 'POST':
+# 		task.content = request.form['content']
 
-	if request.method == 'POST':
-		task.content = request.form['content']
-
-		try:
-			db.session.commit()
-			return redirect('/')
-		except:
-			return 'There was an issue updating your task'
-	else:
-		return render_template('update.html', task=task)
+# 		try:
+# 			db.session.commit()
+# 			return redirect('/')
+# 		except:
+# 			return 'There was an issue updating your task'
+# 	else:
+# 		return render_template('update.html', task=task)
 
 if __name__ == "__main__":
 	# webbrowser.open('http://127.0.0.1:5000/')
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,3 +1,18 @@
+function OnLoad() {
+    // generate all event listeners for double click
+    var x = document.getElementsByClassName('main-done');
+    var y = document.getElementsByClassName('main-err');
+
+    for (var i = 0; i < x.length; i++) {
+        let x_split = x.item(i).id.split('-');
+        let x_id = x_split[x_split.length - 1];
+        document.getElementById(x.item(i).id).addEventListener("dblclick", function() { ShowHideScripts(x_id); }, false);
+        document.getElementById(y.item(i).id).addEventListener("dblclick", function() { ShowHideScripts(x_id); }, false);
+    }
+
+    ShowAll();
+}
+
 function AllError(pt_no) {
     console.log('ErrButton()');
     // making all error-options visible and elongating all relevent rows
@@ -9,6 +24,12 @@ function AllError(pt_no) {
     var y = document.getElementsByClassName('all-error-options-'+pt_no);
     for (var i = 0; i < y.length; i++) {
         y.item(i).style.display = 'table-cell';
+    }
+    if (pt_no != 0) {
+        var z = document.getElementsByClassName('error-option-'+pt_no);
+        for (var i = 0; i < z.length; i++) {
+            z.item(i).style.display = 'table-cell';
+        }
     }
     document.getElementById('all-success-button-'+pt_no).style.backgroundColor = 'white';
     document.getElementById('all-error-button-'+pt_no).style.backgroundColor = 'red';
@@ -37,20 +58,10 @@ function AllDone(pt_no) {
     for (var i = 0; i < z.length; i++) {
         z.item(i).style.display = 'none';
     }
+    // hide all scripts
+    HideScripts(pt_no);    
     ShowHideOrange();
 }
-
-// function dblclickAllDone(pt_no) {
-//     console.log('dblclickAllDone()');
-//     // if hidden, show prescriptions, else, hide
-//     var pt_no = pt_no.toString();
-//     console.log(pt_no);
-//     // if showing
-
-    
-    
-//     ShowHideOrange();
-// }
 
 function ShowHideScripts(pt_no) {
     var z = document.getElementsByClassName(pt_no);
@@ -65,16 +76,8 @@ function ShowHideScripts(pt_no) {
         }
     }
 
-    if (hide) {
-        var x = document.getElementsByClassName(pt_no);
-        for (var i = 0; i < x.length; i++) {
-            x.item(i).style.display = 'none';
-        }
-        var y = document.getElementsByClassName('stay');
-        for (var i = 0; i < y.length; i++) {
-            y.item(i).style.display = 'table-row';
-        }
-    } else {
+    if (hide) {HideScripts(pt_no);} 
+    else {
         var x = document.getElementsByClassName(pt_no);
         for (var i = 0; i < x.length; i++) {
             x.item(i).style.display = 'table-row';
@@ -82,30 +85,14 @@ function ShowHideScripts(pt_no) {
     }
 }
 
-// function dblclickAllError(pt_no) {
-//     console.log('dblclickAllError()');
-//     // if hidden, show prescriptions, else, hide
-//     var pt_no = pt_no.toString();
-//     // show
-//     var x = document.getElementsByClassName(pt_no);
-//     for (var i = 0; i < x.length; i++) {
-//         x.item(i).style.display = 'table-row';
-//     }
-
-//     ShowHideOrange();
-// }
-
-
-function OnLoad() {
-    // generate all event listeners for double click
-    var x = document.getElementsByClassName('main-done');
-    var y = document.getElementsByClassName('main-err');
-
+function HideScripts(pt_no) {
+    var x = document.getElementsByClassName(pt_no);
     for (var i = 0; i < x.length; i++) {
-        let x_split = x.item(i).id.split('-');
-        let x_id = x_split[x_split.length - 1];
-        document.getElementById(x.item(i).id).addEventListener("dblclick", function() { ShowHideScripts(x_id); }, false);
-        document.getElementById(y.item(i).id).addEventListener("dblclick", function() { ShowHideScripts(x_id); }, false);
+        x.item(i).style.display = 'none';
+    }
+    var y = document.getElementsByClassName('stay');
+    for (var i = 0; i < y.length; i++) {
+        y.item(i).style.display = 'table-row';
     }
 }
 
@@ -164,16 +151,56 @@ function ShowHideOrange() {
     }
 }
 
-// function OnLoad() {
-//     var x = document.getElementsByClassName('col-change');
-//     for (var i = 0; i < x.length; i++) {
-//         x.item(i).colSpan = '6';
-//     }
+function CheckCheck(ind, type) {
+    console.log('CheckCheck(pt_no, type): '+ind.toString()+', '+type.toString());
+    var ind = ind.toString();
+    // uncheck all in row but the one just checked
+    var x = document.getElementsByClassName('check-'+ind);
+    for (var i = 0; i < x.length; i++) {
+        if (i != (type-1)) {
+            x.item(i).checked = false;
+        }
+    }
 
-//     var y = document.getElementsByClassName('error-options');
-//     for (var i = 0; i < x.length; i++) {
-//         y.item(i).style.display = 'none';
-//     }    
-// }
+}
 
-console.log('hello');
+function MainCheck(pt_no, type) {
+    console.log('MainCheck(pt_no, type): '+pt_no.toString()+', '+type.toString());
+    var pt_no = pt_no.toString();
+    // uncheck all in row but the one just checked
+    var x = document.getElementsByClassName('pt-check-'+pt_no);
+    for (var i = 0; i < x.length; i++) {
+        if (i != (type-1)) {
+            x.item(i).checked = false;
+        }
+    }
+    // tick same on all scripts
+    var y = document.getElementsByClassName('check-pt-'+pt_no);
+    var checkbox;
+    for (var i = 0; i < y.length; i++) {
+        checkbox = i % 4;
+        if (checkbox != (type-1)) {
+            y.item(i).checked = false;
+        }
+        else {
+            console.log('i: '+i+', type: '+type);
+            y.item(i).checked = true;
+        }
+    }
+    // hide all scripts
+    HideScripts(pt_no);
+}
+
+function ShowAll() {
+    console.log('ShowAll()');
+    var x = document.getElementsByClassName('error-options');
+    for (var i = 0; i < x.length; i++) {
+        x.item(i).style.display = 'table-cell';
+        console.log(i);
+    }
+    var y = document.getElementsByClassName('script');
+    for (var i = 0; i < y.length; i++) {
+        y.item(i).style.display = 'table-row';
+    }
+    ShowHideOrange();
+}
